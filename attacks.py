bb7501d1d9c268122e9d2b01e9c71668bd50c387
m-martin-git <marco.martin@mail.com>
2023-05-23 15:14:02 +0200

    print

--- attacks.py
+++ attacks.py
@@ -79,7 +79,7 @@ def perform_reconnaissance_TCP_ACK(ip_addr="192.168.200."):
         try:
             # Display the list of live hosts with corresponding numbers
             for i, host in enumerate(live_hosts):
-                print(f"{i+1}: {host[i]}")
+                print(f"{i+1}: {host}")
 
             choice = input("Enter the number of the host you want to select: ")
             choice = int(choice)
