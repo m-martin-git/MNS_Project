8809d0913ac350c44b3b4d2a977a09779541c157
m-martin-git <marco.martin@mail.com>
2023-05-24 09:46:54 +0200

    port ask

--- attacks.py
+++ attacks.py
@@ -168,7 +168,19 @@ def perform_reconnaissance_UDP_SCAN(ip_addr="192.168.200."):
     # Print the selected host
     print("Selected host: ", selected_host)
 
-    dst_port = 53
+    # Ask the destination port
+    dst_port = None
+    while not dst_port:
+        try:
+            dst_port = input("Enter the destination port: ")
+            dst_port = int(dst_port)
+
+            if not 1 <= dst_port <= 65535:
+                print("Invalid port. Please enter a valid port number.")
+                dst_port = None
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
     dst_ip = selected_host
     dst_timeout = 10
 
